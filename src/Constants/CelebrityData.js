2f2a63b14ce7d67c2fba5f657a2334466899ef71
shksantosh <santoshking82@gmail.com>
2023-12-20 13:54:35 +0530

    Cleaned up our about us

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,38 @@
+import apj from "../Assets/Images/apj.png";
+import atal from "../Assets/Images/atal.png";
+import billGates from "../Assets/Images/billGates.png";
+import einstein from "../Assets/Images/einstein.png";
+import steveJobs from "../Assets/Images/steveJobs.png";
+
+export const celebrities = [
+    {
+        title: "Atal Bihari Vajpayee",
+        description: "I have a vision of India: an India free of hunger and fear, an India free of illiteracy and want.",
+        image: atal,
+        slideNumber: 1
+    },
+    {
+        title: "APJ Abdul Kamal",
+        description: "Failure will never overtake me if my determination to succeed is strong enough.",
+        image: apj,
+        slideNumber: 2
+    },
+    {
+        title: "Albert Einstein",
+        description: "A person who never made a mistake never tried anything new.",
+        image: einstein,
+        slideNumber: 3
+    },
+    {
+        title: "Steve Jobs",
+        description: "We don't get a chance to do that many things, and every one should be really excellent.",
+        image: steveJobs,
+        slideNumber: 4
+    },
+    {
+        title: "Bill Gates",
+        description: "Success is a lousy teacher. It seduces smart people into thinking they can’t lose.",
+        image: billGates,
+        slideNumber: 5
+    },
+]
